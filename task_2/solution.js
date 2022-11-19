68bcb6793ee1d26996a029cfdf912a637ead24bd
SafonovEgor <e.safonov2015@yandex.ru>
2022-11-19 14:06:21 +0300

    Stage 2 dona

--- task_2/solution.js
+++ task_2/solution.js
@@ -6,14 +6,28 @@ function calcShipping(sum, min, shipping) {
     // Задание №2.1. Рассчитать доставку
 
     // создайте переменную shippingSum
+    let shippingSum;
 
     // если productsSum равно 0,
+    if(productsSum=0); {
+        shippingSum=0;
+    }
     // то shippingSum присвоить значение 0
+
+
 
     // если productsSum Больше или равна freeShippingMinSum,
+    if(productsSum>=freeShippingMinSum) {
+        shippingSum=0;
+    }
     // то shippingSum присвоить значение 0
 
+
+
     // если productsSum больше 0 и меньше freeShippingMinSum,
+    if(productsSum>0 && productsSum<freeShippingMinSum); {
+        shippingSum=shippingPrice;
+    }
     // то shippingSum присвоить значение shippingPrice
 
     // Конец решения задания №2.1.
@@ -29,6 +43,7 @@ function calcDiscount(sum, min, discount) {
     // Задание №2.2. Рассчитать скидку
 
     // создайте переменную discountSum
+    let discountSum;
 
     // если productsSum больше или равно discountMinSum,
     // то присвойте discountSum значение discountPart процентов от productsSum,
